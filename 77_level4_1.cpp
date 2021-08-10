64233e4be7be8af281275f61665220771405ca7d
Prite <P.nutthavat_@hotmail.com>
2021-08-10 17:14:03 +0700

    initial commit

---
+++ 77_level4_1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    while (true) {
+        cout << "Input your number: ";
+        int x;
+        cin >> x;
+        if (x%2 == 0 ){
+            int k = x/2;
+            string arey[k];
+            for (int i=0; i<k;i++) {
+                string ans = "-";
+                for (int j=1; j<x-1; j++) {
+                    ans += "-";
+                }
+                ans[k-1+i] = '*';
+                ans[k-1-i] = '*';
+                arey[i] = ans;
+                cout << ans << endl;    
+            }
+            for (int i=0; i<k; i++){
+                cout << arey[k-1-i] << endl;
+            }
+        }
+        else {
+            int k = (x-1)/2;
+            string arey[k];
+            for (int i=0; i<k+1;i++) {
+                string ans = "-";
+                for (int j=0; j<x-1; j++) {
+                    ans += "-";
+                }
+                ans[k+i] = '*';
+                ans[k-i] = '*';
+                arey[i] = ans;
+                cout << ans << endl;    
+            }
+            for (int i=0; i<k; i++){
+            cout << arey[k-1-i] << endl;
+            }
+        }
+    }
+    return 0;
+}
