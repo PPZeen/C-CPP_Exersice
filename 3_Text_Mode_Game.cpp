b3d89fab7389f780256ae2eb0dd7a2f70e18edff
Prite <P.nutthavat_@hotmail.com>
2021-09-16 03:00:36 +0700

    last version

--- 3_Text_Mode_Game.cpp
+++ 3_Text_Mode_Game.cpp
@@ -34,19 +34,25 @@ char cursor(short x, short y){
     if(!ReadConsoleOutputCharacter(hStd,(LPTSTR)buf,1,c,(LPDWORD)&num_read)){return '\0';}
     else {return buf[0];}
 }
-void generate(int x){
+void generate2(int x){
     int rx,ry, count = 0;
     srand(time(NULL ));
     while (count < x){
         rx = rand();
-        if (rx>9 && rx<71){ 
-            ry = rand();
-            if (ry>1 && ry<6){draw_star(rx,ry); count += 1;}
-        }
+        rx = rx%100;
+        if(rx > 70){rx -= 30;}
+
+        ry = rand();
+        ry = ry%10;
+        if(ry <= 1){ry += 2;}
+        else if(ry > 5){ry -= 4;}
+        
+        if(cursor(rx,ry) != '*'){draw_star(rx,ry); count++;}
     }
 }
+
 int main(){
-    generate(20);
+    generate2(20);
     gotoxy(72,1); cout << "SCORE : 0";
     char ch = ' ';
     int x=38, y=20;
@@ -59,15 +65,15 @@ int main(){
             ch=_getch();
             if(ch == 'a' && x>0) {draw_ship(--x,y);}
             if(ch == 's' && x<67) {draw_ship(++x,y);}
-            if(bullet != 1 && ch == ' ') {bullet=1; bx=x+3; by=y-1;Beep(450,100);}
+            if(bullet != 1 && ch == ' ') {bullet=1; bx=x+3; by=y-1;Beep(450,50);}
             fflush(stdin);
         }
         if (bullet == 1) {
             clear_bullet(bx,by);
             if (by == 2) {bullet = 0;}
             else {
-                if (cursor(bx,by-1) != '*'){draw_bullet(bx,--by);}
-                else {clear_star(bx,by-1); Beep(200,150); bullet = 0; score+=1; gotoxy(72,1); cout << "SCORE : " << score; generate(1);}
+                if (cursor(bx,by-1) != '*'){draw_bullet(bx,--by);Beep(450,50);}
+                else {clear_star(bx,by-1); Beep(200,50); bullet = 0; score+=1; gotoxy(72,1); cout << "SCORE : " << score; generate2(1);}
             }
         }
         Sleep(100);
